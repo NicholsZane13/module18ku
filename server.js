d5b7a602458c36c603915ec82b1f597b65344bae
nicholsz <nichols.zane16@gmail.com>
2023-01-31 19:41:26 -0600

    created connection to mongodb and started routes

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('routes');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost', {
+
+    useNewUrlParser: true,
+    useUnifiedTepology: true
+});
+
+mongoose.set('debug', true);
+
+app.use(routes);
+app.listen(PORT, () => console.log(`Connected to port ${PORT}`));
